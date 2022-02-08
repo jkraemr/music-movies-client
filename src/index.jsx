161a08706a0e610f20a96fbcdfa816cd81c11384
Johannes Krämer <johannes.kraemer@hotmail.com>
2022-02-09 00:13:18 +0100

    Wrap whole app within Container Bootstrap component

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
-
-// Import MainView root component / no file extension needed when importing from js or jsx files in React
 import { MainView } from './components/main-view/main-view';
-
-// Import statement to indicate that ./index.scss needs to be bundled
 import './index.scss';
 
-// Main component (will eventually use all the others)
 class MyMusicMoviesApplication extends React.Component {
   render() {
     return (
-      // Use MainView component via short format (<MainView />) opposed to <MainView></MainView>
-      <MainView />
+      <Container>
+        <MainView />
+      </Container>
     );
   }
 }
