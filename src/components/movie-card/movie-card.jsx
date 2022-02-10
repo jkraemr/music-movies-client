9d90eba89e7eb6ab92153404733ba7a7e2eafe2d
Johannes Krämer <johannes.kraemer@hotmail.com>
2022-02-10 05:27:21 +0100

    Add Bs nav grid ui elements styling to MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './movie-card.scss';
@@ -11,12 +12,13 @@ export class MovieCard extends React.Component {
     const { movie, onMovieClick } = this.props;
 
     return (
-      <Card>
+      // <Card>
+      <Card className="card-columns bg-dark text-white" onClick={() => onMovieClick(movie)} style={{ cursor: "pointer" }}>
         <Card.Img variant="top" src={imgURL + movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+          <Button onClick={() => onMovieClick(movie)} variant="info">Learn more</Button>
         </Card.Body>
       </Card>
     );
@@ -26,7 +28,9 @@ export class MovieCard extends React.Component {
 // propTypes
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired
+    ImagePath: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
